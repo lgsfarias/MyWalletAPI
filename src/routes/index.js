f19372cb4506a31e57d65745ef1ab7647d422666
Vinicius <vinigugelmin@hotmail.com>
2022-05-17 12:48:01 -0300

    Preparando revisão de código

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import auth from './authRoutes.js';
-import transactions from './transactionsRoutes.js';
-
-function init(app) {
-    app.use(auth);
-    app.use(transactions);
-}
-
-export default init;

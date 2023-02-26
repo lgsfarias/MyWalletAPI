51fb55f618a01e6f926ae67a8b5ccfbbdb80230f
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-02-26 19:15:44 -0300

    fix: handle OPTIONS method

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import express, { json } from 'express';
-import cors from 'cors';
 
 import init from './routes/index.js';
 
 const app = express();
 app.use(json());
-app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 init(app);
 
 export default app;

c919ffd301fb14055b18947b230d10a5f77ce95f
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-02-27 11:12:54 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,27 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors';
 
 import init from './routes/index.js';
 
 const app = express();
 app.use(json());
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // handle OPTIONS method
-  if ('OPTIONS' == req.method) {
-    return res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(cors());
 init(app);
 
 export default app;
